b1996ed20798ee14406f511b4c1e689b17338984
Berkay Alparslan <15653983+berkayalparslan@users.noreply.github.com>
2024-09-03 23:52:07 +0200

    slug in post url

--- app/posts/models.py
+++ app/posts/models.py
@@ -1,7 +1,22 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.text import slugify
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.CharField(max_length=1000)
     publish_date = models.DateTimeField("publish date")
+    slug = models.SlugField(null=True, unique=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse("post-detail", kwargs={"slug": self.slug})
+    
--- app/posts/views.py
+++ app/posts/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 def index(request):
     posts = Post.objects.all()
     context = {"posts": posts}
     return render(request, "posts/index.html",context)
+
+def detail(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    context = {'post': post}
+    return render(request, "posts/detail.html",context)
 
