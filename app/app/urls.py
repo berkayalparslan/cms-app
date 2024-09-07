94c23ab5d4c4922928ed5968956361f8a4aff9e3
Berkay Alparslan <15653983+berkayalparslan@users.noreply.github.com>
2024-09-07 23:52:57 +0200

    admin route fix

--- app/app/urls.py
+++ app/app/urls.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('',include('posts.urls'), name='index'),
     path('admin/', admin.site.urls),
+    path('',include('posts.urls'), name='index'),
     path('users/', include('users.urls')),
     path("posts/", include("posts.urls")),
 ]
