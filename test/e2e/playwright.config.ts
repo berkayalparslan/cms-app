e65b5a8564457742d33198677919ff1787f57b25
Berkay Alparslan <15653983+berkayalparslan@users.noreply.github.com>
2024-09-08 23:16:22 +0200

    added ts config

--- test/e2e/playwright.config.ts
+++ test/e2e/playwright.config.ts
@@ -29,6 +29,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    baseURL: 'http://127.0.0.1:8000/'
   },
 
   /* Configure projects for major browsers */
